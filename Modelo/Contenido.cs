91515b3dc65f54a12cc8132ebf5e95b47281f2aa
Hugo Benitez <hugo.boca16@gmail.com>
2024-05-29 15:01:15 -0300

    cambios3

---
+++ Modelo/Contenido.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TPCuatrimestral_Grupo3.Modelo
+{
+    public class Contenido
+    {
+
+        public  int Id { get; set; }
+
+        public string Titulo { get; set; }
+
+        public int IdCategoria { get; set; }
+
+        public int IdOrigen { get; set; }
+
+        public int IdTipoContenido { get; set; }
+
+        public int IdPlataforma { get; set; }
+
+        public DateTime FechaLanzamiento { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public decimal EstrellasPromedio { get; set; }
+
+     
+    /*
+    Foreign key(IdCategoria) references Categoria(Id),
+    Foreign key(IdOrigen) references Paises(Id),
+    Foreign key(IdTipoContenido) references TiposContenidos(Id)
+    */
+
+    }
+}
---
+++ Modelo/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TPCuatrimestral_Grupo3.Modelo
+{
+    public class Usuario
+    {
+
+        public int ID { get; set; }
+
+        public string NombreUsuario { get; set; }
+
+        public string Contrasena { get; set; }
+
+        public string Email { get; set; }
+
+        public bool EsAdministrador { get; set; }
+
+        public bool EsVip { get; set; }
+    
+    }
+}
