1b97a2f1bd037630c4dabb632e71b7f432011eaa
Hugo Benitez <hugo.boca16@gmail.com>
2024-05-30 11:56:39 -0300

    cambios7

---
+++ Modelo/Resena.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+
+namespace TPCuatrimestral_Grupo3.Modelo
+{
+    public class Resena
+    {
+
+        public int Id { get; set; }
+
+        public int IdContenido { get; set; }
+
+        public int IdUsuario { get; set; }
+
+        public int Estrellas { get; set; }
+
+        public string Texto { get; set; }
+
+        public bool EsAprobada { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+
+        /*Id bigint identity(1,1) not null Primary key,
+        IdContenido int not null,
+        IdUsuario int not null,
+        Estrellas int check(Estrellas>=1 and Estrellas<=5),
+        Texto text not null,
+        EsAprobada bit default 0,
+        FechaCreacion date not null,
+        Foreign key(IdContenido) references Contenidos(Id),
+        Foreign key(IdUsuario) references Usuarios(Id)*/
+    }
+}
