fff5d6fbc3a826463f6a793d850867d0ed4a311e
jonatan <jonatan_lazarte1992@hotmail.com>
2024-06-29 01:59:32 -0300

    conexiones

---
+++ Modelo/PlataformaXPelicula.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TPCuatrimestral_Grupo3.Modelo
+{
+    public class PlataformaXPelicula
+    {
+        public short IdPlataforma { get; set; }
+        public string NombrePlataforma { get; set; }
+        public long IdPelicula { get; set; }
+        public string  TituloPelicula { get; set; }
+
+    }
+}
---
+++ negocio/PlataformaXPeliculaNegocio.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TPCuatrimestral_Grupo3.Modelo;
+
+namespace TPCuatrimestral_Grupo3.Negocio
+{
+    public class PlataformaXPeliculaNegocio
+    {
+        public List<PlataformaXPelicula> listarConSP()
+        {
+            List<PlataformaXPelicula> lista = new List<PlataformaXPelicula>();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+
+                datos.setearProcedimiento("PlataformaPelicula");
+                datos.ejecutarLectura();
+                while (datos.Lector.Read())
+                {
+                    PlataformaXPelicula aux = new PlataformaXPelicula();
+
+                    aux.IdPlataforma = (short)datos.Lector["IdPlataforma"];
+                    aux.NombrePlataforma = (string)datos.Lector["Plataforma"];
+                    aux.IdPelicula = (long)datos.Lector["IdContenido"];
+                    aux.TituloPelicula = (string)datos.Lector["Titulo"];
+
+
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
